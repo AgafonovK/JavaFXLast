e66a86cff365a4e4fdcc4bfb0bad8b95c271f48a
AgafonovK <konstantinagaf@gmail.com>
2018-03-11 22:11:55 +0500

    Add second fxml. But don't add this fxml.

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -5,19 +5,31 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
+//@SpringBootApplication
 public class Main extends Application {
+
+    protected ConfigurableApplicationContext springContext;
+   // protected StageManager stageManager;
+
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+
+        Parent root = FXMLLoader.load(getClass().getResource("/sample.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
     }
 
 
     public static void main(String[] args) {
+
+
         launch(args);
     }
+
+
 }
